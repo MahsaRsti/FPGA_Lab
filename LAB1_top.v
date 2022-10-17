9ffedc588bd7134dcf844774ac27412fee18e90b
Mahsa Rasti <mahsa.rasti80@gmail.com>
2022-10-17 14:57:14 +0330

    top module needs to be completed. cntrl ha ham ehtemalan kamel nistand va signal hashoon kame.

--- LAB1_top.v
+++ LAB1_top.v
@@ -15,6 +15,13 @@ parameter Oversampling = 4;
 
 ////////////////////////////////
 //Place your controller here
+transmitter_cntrlr TX_CU(.clk(CLOCK_50),.rst(KEY),output_valid,TX_busy,TX_start);
+
+reciever_cntrlr RX_CU(.clk(CLOCK_50),.rst(KEY),data_ready,input_valid,output_valid);
+
+ async_transmitter TX(.clk(CLOCK_50),TxD_start,TxD_data,TxD,TxD_busy);
+
+async_receiver RX(.clk(CLOCK_50),RxD,RxD_data_ready = 0,RxD_data = 0);
 ////////////////////////////////
 
 endmodule
