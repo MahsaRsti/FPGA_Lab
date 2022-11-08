a19a0d5b5795c0979b5832ee716dba2e24b60c2d
Mahsa Rasti <mahsa.rasti80@gmail.com>
2022-11-08 16:56:09 +0330

    g.

--- TB.v
+++ TB.v
@@ -1,4 +1,83 @@
-module TB
+module TB(CLOCK_50, KEY, SW, UART_RXD, UART_TXD, LEDR);
+
+input CLOCK_50;
+input [0:0] KEY;		//reset SW
+input [3:0] SW;
+output [15:0] LEDR;
+
+input UART_RXD;
+output UART_TXD;
+
+parameter ClkFrequency = 50000000;
+parameter Baud = 115200;
+parameter Oversampling = 4;
+
+
+////////////////////////////////
+//Place your controller here
+// transmitter_cntrlr TX_CU(.clk(CLOCK_50),.rst(SW[0]),output_valid,TX_busy,TX_start);
+reg [15:0] ff1, ff2;
+reg [7:0] uart_out_reg;
+wire [7:0] uart_in,uart_out;
+wire input_valid,output_valid,TxD_busy,TxD_start,RxD_ready;
+wire [15:0]FIR_input;
+wire [37:0]FIR_output;
+ wire ff1_Sel,ff2_Sel,ff2_load,ff1_load;
+
+FIR #(16,38,64,6) FF(CLOCK_50,SW[0],input_valid,FIR_input,output_valid,FIR_output);
+trn_cu  CU1 (TxD_busy,CLOCK_50,SW[0],output_valid,ff2_Sel,ff2_load,TxD_start);
+rec_cu  CU2 (RxD_ready,CLOCK_50,SW[0],ff1_Sel,ff1_load,input_valid);
+async_receiver AR(
+    SW[0],
+    CLOCK_50,
+    UART_RXD,
+    RxD_ready,
+    uart_in  // data received, valid only (for one clock cycle) when RxD_data_ready is asserted
+);
+
+async_transmitter AT(
+    CLOCK_50,
+    TxD_start,
+    uart_out,
+    UART_TXD,
+    TxD_busy
+);
+
+ //FIR 
+ always @(*) begin
+    if(SW[0]) begin ff1 <= 0; ff2 <= 0; end
+    else begin
+        if(ff1_load)begin
+            if(ff1_Sel)
+                ff1[15:8]=uart_in;
+            else
+                ff1[7:0]=uart_in;
+        end
+        if(ff2_load)
+            ff2=FIR_output[23:8];
+        if(ff2_Sel)
+            uart_out_reg=ff2[15:8];
+        else
+            uart_out_reg=ff2[7:0];
+    end
+    
+ end
+
+assign uart_out=uart_out_reg;
+assign LEDR=ff1;
+assign FIR_input=ff1;
+// async_receiver RX(.clk(CLOCK_50),RxD,RxD_data_ready = 0,RxD_data = 0);
+////////////////////////////////
+
+endmodule
+
+
+
+
+
+
+
+/*module TB1
   (
     ////////////////////  Clock Input     ////////////////////   
     CLOCK_27,            //  27 MHz
@@ -264,9 +343,10 @@ wire [7:0] TxD_data;//=8'h44;
     .clock(CLOCK_50)
 );*/
 
-async_transmitter TX(.clk(CLOCK_50),.TxD_start(SW[0]),.TxD_data(TxD_data),.TxD(UART_TXD),.TxD_busy(LEDR[2]));
-async_receiver RX(.rst(SW[1]),.clk(CLOCK_50),.RxD(UART_RXD),.RxD_data_ready(LEDR[3]),.RxD_data(TxD_data));
-assign LEDR[1] = SW[2] | SW[1];
+//async_transmitter TX(.clk(CLOCK_50),.TxD_start(SW[0]),.TxD_data(TxD_data),.TxD(UART_TXD),.TxD_busy(LEDR[2]));
+//async_receiver RX(.rst(SW[1]),.clk(CLOCK_50),.RxD(UART_RXD),.RxD_data_ready(LEDR[3]),.RxD_data(TxD_data));
+/*assign LEDR[1] = SW[2] | SW[1];
 // assign LEDR[17:10]=RxD_data;
 
 endmodule
+*/
