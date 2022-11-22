0a133f33a74e45a160262f57eb5acb4d3a687ed5
Mahsa Rasti <mahsa.rasti80@gmail.com>
2022-11-22 10:23:05 +0330

    lab2 added.

---
+++ Lab#2/Hello_World/obj/default/globals.d
@@ -0,0 +1,90 @@
+obj/default/globals.o: globals.c globals.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_parallel_port.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_dev.h ../Hello_World_bsp/system.h \
+  ../Hello_World_bsp/linker.h ../Hello_World_bsp//HAL/inc/sys/alt_llist.h \
+  ../Hello_World_bsp//HAL/inc/alt_types.h \
+  ../Hello_World_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_llist.h \
+  ../Hello_World_bsp//HAL/inc/alt_types.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_alarm.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_llist.h \
+  ../Hello_World_bsp//HAL/inc/priv/alt_alarm.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_warning.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h \
+  ../Hello_World_bsp//HAL/inc/io.h \
+  ../Hello_World_bsp//HAL/inc/alt_types.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_character_lcd.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_audio.h \
+  ../Hello_World_bsp//HAL/inc/alt_types.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_dev.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_ps2.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+  ../Hello_World_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_stdio.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_irq.h \
+  ../Hello_World_bsp//HAL/inc/nios2.h \
+  ../Hello_World_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../Hello_World_bsp/system.h ../Hello_World_bsp//HAL/inc/nios2.h \
+  ../Hello_World_bsp//HAL/inc/sys/alt_irq.h
+
+globals.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_parallel_port.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_dev.h:
+
+../Hello_World_bsp/system.h:
+
+../Hello_World_bsp/linker.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_llist.h:
+
+../Hello_World_bsp//HAL/inc/alt_types.h:
+
+../Hello_World_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_llist.h:
+
+../Hello_World_bsp//HAL/inc/alt_types.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_llist.h:
+
+../Hello_World_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_warning.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_parallel_port_regs.h:
+
+../Hello_World_bsp//HAL/inc/io.h:
+
+../Hello_World_bsp//HAL/inc/alt_types.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_character_lcd.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../Hello_World_bsp//HAL/inc/alt_types.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_dev.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../Hello_World_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_stdio.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_irq.h:
+
+../Hello_World_bsp//HAL/inc/nios2.h:
+
+../Hello_World_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../Hello_World_bsp/system.h:
+
+../Hello_World_bsp//HAL/inc/nios2.h:
+
+../Hello_World_bsp//HAL/inc/sys/alt_irq.h:
